ce35bf06719b82d2b96bed182cb147ecdf7e6e15
omersaskin <pc.omer.saskin@gmail.com>
2023-09-06 10:19:09 +0300

    MongoDB bağlantısının yapılması
    https://cloud.mongodb.com

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-app.listen(8800, ()=> {
-    console.log("Backend server is running!")
-})
+dotenv.config();
+
+// Connect to MongoDB using async/await
+async function connectToMongo() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectToMongo();
+
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+});

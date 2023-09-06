e807025cc765755c75c60eb9d7a1fe38b102dcb5
omersaskin <pc.omer.saskin@gmail.com>
2023-09-06 10:35:43 +0300

    Middlewares & First Request eklendi

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ async function connectToMongo() {
 
 connectToMongo();
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to homepage")
+})
+
+app.get("/users", (req, res) => {
+    res.send("Welcome to user page")
+})
+
 app.listen(8800, () => {
     console.log("Backend server is running!");
 });
